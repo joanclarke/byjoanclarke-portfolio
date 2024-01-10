e7b0ef901f7aa8214abf52a58dae010a4f3e17bd
joanclarke <jcjuju2014@gmail.com>
2024-01-10 03:45:29 -0500

    build Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './app.scss'
+import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
-import Test from './components/navbar/Test'
 
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services">Parallax</section>
       <section>Services</section>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
+import Sidebar from '../sidebar/Sidebar'
 import './navbar.scss'
 import { motion } from 'framer-motion'
-import Sidebar from './sidebar/Sidebar'
+
 const Navbar = () => {
   return (
     <div className="navbar">
