8f711eab2c4a443e8e355045115e4e3f5f88be77
joanclarke <jcjuju2014@gmail.com>
2024-01-18 20:52:32 -0500

    animate Services section using useInView hook

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,31 +1,62 @@
+import { useRef } from 'react'
 import './services.scss'
-import { motion } from 'framer-motion'
+import { motion, useInView } from 'framer-motion'
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+}
 
 const Services = () => {
+  const ref = useRef()
+
+  const isInView = useInView(ref, { margin: '-100px' })
+
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={isInView && 'animate'}
+    >
+      <motion.div className="textContainer" variants={variants}>
         <p>
           I focus on helping your brand grow
           <br /> and move forward
           <hr />
         </p>
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="people.webp" alt="people" />
           <h1>
-            <p>Unique</p> Ideas
+            <motion.b whileHover={{ color: 'orange' }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <p>For Your</p> Business.
+            <motion.b whileHover={{ color: 'orange' }}>For Your</motion.b>{' '}
+            Business.
           </h1>
           <button>WHAT I DO</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div
           className="box"
           whileHover={{ background: 'lightgray', color: 'black' }}
