3d414ec6b6f79ab9239b3ee5c0b71f2c165f9d02
joanclarke <jcjuju2014@gmail.com>
2024-01-21 23:31:47 -0500

    create contact component and style fil

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,32 @@
+import './contact.scss'
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="textContainer">
+        <h2>Let's work together</h2>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>hello@react.dev</span>
+        </div>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>hello@react.dev</span>
+        </div>
+        <div className="item">
+          <h2>Phone</h2>
+          <span>+1 234 567 8910</span>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" required placeholder="Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea rows={8} placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+export default Contact
