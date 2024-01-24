a0daa3f44874955e15d510db8d2e1b51f57db4f4
joanclarke <jcjuju2014@gmail.com>
2024-01-23 23:43:15 -0500

    add functionality to contact using the Emailjs library

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 import './contact.scss'
 import { motion, useInView } from 'framer-motion'
-// import emailjs from '@emailjs/browser'
+import emailjs from '@emailjs/browser'
 
 const variants = {
   initial: {
@@ -20,8 +20,31 @@ const variants = {
 
 const Contact = () => {
   const ref = useRef()
+  const formRef = useRef()
+  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const isInView = useInView(formRef, { margin: '-100px' })
 
-  const isInView = useInView(ref, { margin: '-100px' })
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_ubqvh8g',
+        'template_e69350j',
+        formRef.current,
+        '1c1nT6GD7hE5erdMf'
+      )
+      .then(
+        (result) => {
+          setSuccess(true)
+        },
+        (error) => {
+          setError(true)
+        }
+      )
+  }
+
   return (
     <motion.div
       ref={ref}
@@ -68,14 +91,18 @@ const Contact = () => {
           </svg>
         </motion.div>
         <motion.form
+          ref={formRef}
+          onSubmit={sendEmail}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 4, duration: 1 }}
         >
-          <input type="text" required placeholder="Name" />
-          <input type="email" required placeholder="Email" />
-          <textarea rows={8} placeholder="Message" />
+          <input type="text" required placeholder="Name" name="name" />
+          <input type="email" required placeholder="Email" name="email" />
+          <textarea rows={8} placeholder="Message" name="message" />
           <button>Submit</button>
+          {error && 'Error'}
+          {success && 'Success'}
         </motion.form>
       </div>
     </motion.div>
